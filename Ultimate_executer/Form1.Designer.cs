b38b929807b4037cf6dbe5e0d0f543879e5913b8
Mark Faust Kou <chuyun0316@hotmail.com>
2019-12-10 09:03:52 +0900

    Add project files.

---
+++ Ultimate_executer/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Ultimate_executer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Set EnergyPlus Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(240, 19);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(240, 60);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 25);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 57);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Set IDFs folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(240, 100);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(331, 25);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(20, 97);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Set EPWs folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(240, 140);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(331, 25);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(20, 137);
+            this.button4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(210, 32);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Set output folder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(255, 180);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(163, 22);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "run Expandobject";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(473, 172);
+            this.button5.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 32);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "GO!!!";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(123, 173);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(104, 26);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 179);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "CPU cores";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 210);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(551, 131);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 357);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "EnergyPlus Ultimate Executer";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Ultimate_executer/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ultimate_executer
+{
+    public partial class Form1 : Form
+    {
+        string ep_path;
+        string idf_folder;
+        string[] idfs;
+        string epw_folder;
+        string[] epws;
+        string output_folder;
+        bool expobj;
+
+        bool ep_path_ok;
+        bool idfs_ok;
+        bool epws_ok;
+        bool output_folder_ok;
+
+        List<string[]> paras;
+
+        private string EP_Path
+        {
+            set
+            {
+                if (File.Exists(value + @"\energyplus.exe") && File.Exists(value + @"\Energy+.idd") && File.Exists(value + @"\ExpandObjects.exe") && File.Exists(value + @"\PostProcess\ReadVarsESO.exe"))
+                {
+                    ep_path = value;
+                    ep_path_ok = true;
+                }
+            }
+            get
+            {
+                if (ep_path_ok) { return ep_path; }
+                else { return "EnergyPlus path Error"; }
+            }
+        }
+
+        private string IDF_FOLDER
+        {
+            set
+            {
+                if (Directory.Exists(value))
+                {
+                    idf_folder = value;
+                    idfs = Directory.GetFiles(idf_folder, "*.idf");
+                    idfs_ok = idfs.Length > 0;
+                }
+            }
+            get
+            {
+                if (idfs_ok) { return idf_folder; }
+                else { return "IDFs folder Error"; }
+            }
+        }
+
+        private string EPW_FOLDER
+        {
+            set
+            {
+                if (Directory.Exists(value))
+                {
+                    epw_folder = value; 
+                    epws = Directory.GetFiles(epw_folder, "*.epw");
+                    epws_ok = epws.Length > 0;
+                }
+            }
+            get
+            {
+                if (epws_ok) { return epw_folder; }
+                else { return "EPWs folder Error"; }
+            }
+        }
+
+        private string OUTPUT_FOLDER
+        {
+            set
+            {
+                if (Directory.Exists(value))
+                {
+                    output_folder = value;
+                    output_folder_ok = true;
+                }
+            }
+            get
+            {
+                if (output_folder_ok) { return output_folder; }
+                else { return "Output path Error"; }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FResize();
+
+            ep_path = "";
+            idf_folder = "";
+            epw_folder = "";
+            output_folder = "";
+            expobj = checkBox1.Checked;
+
+            ep_path_ok = false;
+            idfs_ok = false;
+            epws_ok = false;
+            output_folder_ok = false;
+
+            comboBox1.Items.Add("ALL");
+
+            for (int i = 1; i < 33; i++)
+            {
+                comboBox1.Items.Add(i.ToString());
+            }
+
+            comboBox1.SelectedIndex = 0;
+            comboBox1.Enabled = false;
+            checkBox1.Checked = true;
+
+            richTextBox1.Text = "";
+
+            paras = new List<string[]>();
+
+            //textBox1.Text = @"C:\EnergyPlusV8-8-0";
+            //textBox2.Text = @"C:\FTP\JPmodels\Ultimate_demo";
+            //textBox3.Text = @"C:\FTP\JPmodels";
+            //textBox4.Text = @"C:\temp";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            EP_Path = textBox1.Text;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = fbd.SelectedPath;
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            IDF_FOLDER = textBox2.Text;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = fbd.SelectedPath;
+            }
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            EPW_FOLDER = textBox3.Text;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = fbd.SelectedPath;
+            }
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            OUTPUT_FOLDER = textBox4.Text;
+        }
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            expobj = checkBox1.Checked;
+        }
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+
+            checkBox1.Enabled = false;
+            comboBox1.Enabled = false;
+
+            richTextBox1.Text = "";
+
+            if (!ep_path_ok) { richTextBox1.Text += EP_Path + Environment.NewLine; }
+            if (!idfs_ok) { richTextBox1.Text += IDF_FOLDER + Environment.NewLine; }
+            if (!epws_ok) { richTextBox1.Text += EPW_FOLDER + Environment.NewLine; }
+            if (!output_folder_ok) { richTextBox1.Text += OUTPUT_FOLDER + Environment.NewLine; }
+
+            if (ep_path_ok && idfs_ok && epws_ok && output_folder_ok)
+            {
+                richTextBox1.Text += "Making task queue" + Environment.NewLine;
+                List<EP_run> tasks = new List<EP_run>();
+                foreach (string epw in epws)
+                {
+                    foreach (string idf in idfs)
+                    {
+                        tasks.Add(new EP_run(ep_path, idf, epw, output_folder, expobj));
+                    }
+                }
+
+                richTextBox1.Text += "Creating temp folders" + Environment.NewLine;
+                Directory.CreateDirectory(output_folder + @"\temp\");
+                Directory.CreateDirectory(output_folder + @"\Warnings\");
+                Directory.CreateDirectory(output_folder + @"\Errors\");
+
+                foreach(string epw in epws)
+                {
+                    Directory.CreateDirectory(String.Format(@"{0}\{1}\", output_folder, Path.GetFileNameWithoutExtension(epw)));
+                }
+
+                foreach (EP_run task in tasks) { task.Creat_Temp_Folder(richTextBox1); }
+
+                richTextBox1.Text += Environment.NewLine;
+
+                richTextBox1.Text += "Simulation starts ... " + Environment.NewLine;
+                Directory.SetCurrentDirectory(ep_path);
+                Parallel.ForEach(tasks, (item, loopState) => { item.Run(); });
+
+                richTextBox1.Text += Environment.NewLine;
+
+                richTextBox1.Text += "Converting ESO to CSV" + Environment.NewLine;
+                foreach (EP_run task in tasks) { task.ReadVarEso(richTextBox1); }
+
+                richTextBox1.Text += Environment.NewLine + "ALL DONE";
+            }
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+
+            checkBox1.Enabled = true;
+            comboBox1.Enabled = false;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            FResize();
+        }
+        private void FResize()
+        {
+            textBox1.Width = this.Width - 175;
+            textBox2.Width = this.Width - 175;
+            textBox3.Width = this.Width - 175;
+            textBox4.Width = this.Width - 175;
+
+            richTextBox1.Height = this.Height - 192;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+    }
+
+    public class EP_run
+    {
+        string ep_path;
+        string idf;
+        string epw;
+        string out_path;
+        bool expobj;
+        public string report;
+
+        string idf_name;
+        string epw_name;
+        string local_temp_path;
+        public EP_run(string _ep_path, string _idf_path, string _epw_path, string _out_path, bool _expobj)
+        {
+            ep_path = _ep_path;
+            idf = _idf_path;
+            epw = _epw_path;
+            out_path = _out_path;
+            expobj = _expobj;
+
+            idf_name = Path.GetFileNameWithoutExtension(idf);
+            epw_name = Path.GetFileNameWithoutExtension(epw);
+            local_temp_path = String.Format(@"{0}\temp\{1}.{2}\", out_path, idf_name, epw_name);
+        }
+
+        public void Creat_Temp_Folder(RichTextBox rtb)
+        {
+            Directory.CreateDirectory(local_temp_path);
+            File.Copy(idf, local_temp_path + "in.idf", true);
+
+            rtb.Text += String.Format("{0}.{1} created", idf_name, epw_name) + Environment.NewLine;
+
+            if (expobj)
+            {
+                ProcessStartInfo eob_info = new ProcessStartInfo(ep_path + @"\ExpandObjects.exe");
+                eob_info.UseShellExecute = true;
+                eob_info.WorkingDirectory = local_temp_path;
+                eob_info.CreateNoWindow = true;
+                eob_info.WindowStyle = ProcessWindowStyle.Hidden;
+
+                Process expobject = Process.Start(eob_info);
+                expobject.WaitForExit();
+                expobject.Close();
+                if (File.Exists(local_temp_path + "expanded.idf"))
+                {
+                    if(File.Exists(local_temp_path + "_in._idf")) { File.Delete(local_temp_path + "_in._idf"); }
+                    File.Move(local_temp_path + "in.idf", local_temp_path + "_in._idf");
+                    File.Move(local_temp_path + "expanded.idf", local_temp_path + "in.idf");
+                    rtb.Text += String.Format("{0} expamded", idf_name) + Environment.NewLine;
+                }
+            }
+        }
+        public void Run()
+        {
+            ProcessStartInfo run_info = new ProcessStartInfo(ep_path + @"\energyplus.exe");
+            run_info.UseShellExecute = true;
+            run_info.WorkingDirectory = local_temp_path;
+
+            run_info.Arguments = "-w " + epw;
+            run_info.Arguments += " -p eplusout";
+            run_info.Arguments += " -d " + local_temp_path;
+            run_info.Arguments += " -s D ";
+            run_info.Arguments += local_temp_path + @"\in.idf";
+
+            Process runep = Process.Start(run_info);
+            runep.WaitForExit();
+            runep.Close();
+
+            string[] temp_files = Directory.GetFiles(local_temp_path);
+            foreach (string f in temp_files)
+            {
+                string extension = Path.GetExtension(f);
+                if (extension != ".err" && extension != ".eso") 
+                { 
+                    File.Delete(f); 
+                }
+            }
+        }
+
+        public void ReadVarEso(RichTextBox rtb)
+        {
+            ProcessStartInfo rve_info = new ProcessStartInfo(ep_path + @"\PostProcess\ReadVarsESO.exe");
+            rve_info.UseShellExecute = true;
+            rve_info.WorkingDirectory = local_temp_path;
+            rve_info.CreateNoWindow = true;
+            rve_info.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process rve = Process.Start(rve_info);
+            rve.WaitForExit();
+            rve.Close();
+
+            bool done = false;
+
+            if (File.Exists(local_temp_path + "eplusout.csv"))
+            {
+                if (new FileInfo(local_temp_path + "eplusout.csv").Length > 10) { done = true; }
+            }
+
+            if (done)
+            {
+                string t0 = String.Format(@"{0}\Warnings\{1}.{2}.err", out_path, idf_name, epw_name);
+                File.Copy(local_temp_path + "eplusout.err", t0, true);
+                string t1 = String.Format(@"{0}\{1}\{2}.csv", out_path, epw_name, idf_name);
+                File.Copy(local_temp_path + "eplusout.csv", t1, true);
+                report = String.Format("{0}.{1}: Successfully", idf_name, epw_name);
+            }
+            else
+            {
+                File.Copy(local_temp_path + "eplusout.err", String.Format(@"{0}\Errors\{1}.{2}.err", out_path, idf_name, epw_name), true);
+                report = String.Format("{0}.{1}: Errors happen", idf_name, epw_name);
+            }
+
+            string[] temp_files = Directory.GetFiles(local_temp_path);
+            foreach (string f in temp_files) { File.Delete(f); }
+
+            rtb.Text += report + Environment.NewLine;
+        }
+    }
+}
